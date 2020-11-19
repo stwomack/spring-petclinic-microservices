d6d56baeb6ab9bb2f6d33fa44df7f210f95e11e3
Tim Hess <thess@vmware.com>
2020-11-19 10:51:24 -0500

    add some powershell and a .sln for all .net things

---
+++ deploy-to-asc.ps1
@@ -0,0 +1,82 @@
+param (
+    [String]$location = "eastus2",
+    [String]$rg = "Steeltoe",
+    [String]$ASCServiceInstance = "steeltoe",
+    [switch]$initializeAzureSpringCloud,
+    [switch]$buildJava,
+    [switch]$buildDotNet,
+    [switch]$provisionAppSlots,
+    [switch]$deployAllApps,
+    [switch]$deployJavaApps,
+    [switch]$deployDotNetApps,
+    [String]$configServerGitUri = "https://github.com/steeltoeoss-incubator/spring-petclinic-microservices-config"
+)
+
+$TotalTime = New-Object -TypeName System.Diagnostics.Stopwatch
+$TotalTime.Start()
+
+# include some helper functions that should make this file easier to read
+. .\deployment-functions.ps1
+
+if ($initializeAzureSpringCloud)
+{
+    az extension add --name spring-cloud
+    az group create --location $location --name $rg
+    az spring-cloud create --name $ASCServiceInstance -g $rg
+
+    Write-Host "Setting up config server"
+    az spring-cloud config-server git set --name "$ASCServiceInstance" --uri "$configServerGitUri" --label "azure-spring-cloud"
+}
+
+az configure --defaults group=$rg
+az configure --defaults spring-cloud=$ASCServiceInstance
+
+if ($provisionAppSlots)
+{
+    ASCCreateApp "spring-admin-server" "Java_8"
+    # ASCCreateApp "customers-service" "Java_8"
+    ASCCreateApp "customers-service" "NetCore_31"
+    # ASCCreateApp "vets-service" "Java_8"
+    ASCCreateApp "vets-service" "NetCore_31"
+    # ASCCreateApp "visits-service" "Java_8"
+    ASCCreateApp "visits-service" "NetCore_31"
+    ASCCreateApp "spring-api-gateway" "Java_8" "--is-public"
+}
+
+if ($buildJava)
+{
+    #PublishJavaApp "customers-service"
+    #PublishJavaApp "vets-service"
+    #PublishJavaApp "visits-service"
+    PublishJavaApp "api-gateway"
+    PublishJavaApp "admin-server"
+}
+
+if ($buildDotNet)
+{
+    PublishDotNetApp "customers"
+    PublishDotNetApp "vets"
+    PublishDotNetApp "visits"
+}
+
+if ($deployDotNetApps -or $deployAllApps)
+{
+    ASCDeployDotNet "customers" $false
+    ASCDeployDotNet "vets" $false
+    ASCDeployDotNet "visits" $false
+}
+
+if ($deployJavaApps -or $deployAllApps)
+{
+    # ASCDeployJar "customers-service"
+    # ASCDeployJar "vets-service"
+    # ASCDeployJar "visits-service"
+    Write-Host "Deploying spring admin server"
+    ASCDeployJar "admin-server" $false
+
+    Write-Host "Deploying api gateway to default environment"
+    ASCDeployJar "api-gateway" $true
+}
+
+$TotalTime.Stop()
+Write-Host "Total processing time:" $TotalTime.Elapsed.ToString()
---
+++ deployment-functions.ps1
@@ -0,0 +1,71 @@
+function PublishDotNetApp
+{
+    param($ServiceName)
+    
+    Write-Host "---"
+    Write-Host "Building Steeltoe $ServiceName service"
+    dotnet publish -c release -o "steeltoe-petclinic-$ServiceName-service/target" "steeltoe-petclinic-$ServiceName-service/src/main/steeltoe-petclinic-$ServiceName-api.csproj"
+}
+
+function PublishJavaApp
+{
+    param($ServiceName)
+
+    Write-Host "---"
+    Write-Host "Building $ServiceName"
+    ./mvnw clean package -pl "spring-petclinic-$ServiceName" -am
+}
+
+function ASCCreateApp {
+    param ($AppName, $Runtime, $ExtraParams)
+    
+    Write-Host "---"
+    Write-Host "Creating a home for $AppName with runtime $Runtime"
+    Start-Process -FilePath "az" -ArgumentList "spring-cloud app create --name $AppName --runtime-version $Runtime $ExtraParams"
+}
+
+function ASCDeployDotNet {
+    param ($ServiceName, [bool]$RunInForeground)
+
+    Write-Host "---"
+    ASCWaitForReady "$ServiceName-service"
+    Write-Host "Deploying .NET service $ServiceName"
+    if ($RunInForeground)
+    {
+        az spring-cloud app deploy --name $ServiceName-service --runtime-version NetCore_31 --main-entry $ServiceName-service.dll --artifact-path steeltoe-petclinic-$ServiceName-service/src/main/deploy.zip
+    }
+    else
+    {
+        Start-Process -FilePath "az" -ArgumentList "spring-cloud app deploy --name $ServiceName-service --runtime-version NetCore_31 --main-entry $ServiceName-service.dll --artifact-path steeltoe-petclinic-$ServiceName-service/src/main/deploy.zip"
+    }
+}
+
+function ASCDeployJar {
+    param($ServiceName, [bool]$RunInForeground)
+
+    Write-Host "---"
+    ASCWaitForReady "spring-$ServiceName"
+    if ($RunInForeground)
+    {
+        az spring-cloud app deploy --name "spring-$ServiceName" --jar-path "spring-petclinic-$ServiceName/target/spring-petclinic-$ServiceName-2.3.2.jar" --verbose
+    }
+    else
+    {
+        Start-Process -FilePath "az" -ArgumentList "spring-cloud app deploy --name spring-$ServiceName --jar-path spring-petclinic-$ServiceName/target/spring-petclinic-$ServiceName-2.3.2.jar --verbose"
+    }
+}
+
+function ASCWaitForReady{
+    param($ServiceName)
+    Write-Host "Waiting for $ServiceName to be ready..."
+    $appstate = az spring-cloud app show --name $ServiceName | ConvertFrom-Json
+    $status = $appstate.properties.provisioningState
+    while ($status -ne "Succeeded")
+    {
+        Write-Host "Waiting for a successful provisioning status... Currently $status. Checking again in 5 seconds"
+        Start-Sleep -s 5
+        $appstate = az spring-cloud app show --name $ServiceName | ConvertFrom-Json
+        $status = $appstate.properties.provisioningState
+    }
+    Write-Host "App Instance is ready for a deployment"
+}
