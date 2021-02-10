8d2cbc0308da0f14aa7f0318c5368ca9cd85d9f6
Tim Hess <thess@vmware.com>
2021-02-10 09:43:40 -0500

    remove dockerize, simplify image build process

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayController.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayController.java
@@ -43,7 +43,7 @@ public class ApiGatewayController {
 
     private final VisitsServiceClient visitsServiceClient;
 
-    private final ReactiveCircuitBreakerFactory cbFactory;
+    private final ReactiveCircuitBreakerFactory<?, ?> cbFactory;
 
     @GetMapping(value = "owners/{ownerId}")
     public Mono<OwnerDetails> getOwnerDetails(final @PathVariable int ownerId) {
